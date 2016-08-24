f4133f8e43bdfd6b7860e86a05bc9c7a48cf677e
James_B <aesk.99@gmail.com>
2016-08-25 11:45:18 +1200

    cardshop in progress

--- client/components/cardShop.js
+++ client/components/cardShop.js
@@ -1,13 +1,34 @@
 import React from 'react'
 const _ = require('lodash')
 
+const CardButton = (props) => {
+  const {card, clickHandler} = props
+
+  return (
+    <button
+      onClick={ () => { clickHandler(card.name, card.price) }}
+    >{card.name}</button>
+  )
+}
+
 class CardShop extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.buyClick = this.props.buyCard.bind(this)
+    this.sellClick = this.props.sellCard.bind(this)
+  }
 
   render (){
     console.log(this.props);
     return (
       <div>
-        
+        {
+          _.map(cards, card => (<CardButton key={card.id} card={card} clickHandler={this.buyClick} />))
+        }
+        {
+          _.map(cards, card => (<CardButton key={card.id} card={card} clickHandler={this.sellClick} />))
+        }
       </div>
     )
   }
--- client/state/initialstate.js
+++ client/state/initialstate.js
@@ -1,10 +1,10 @@
 
 const initialState = {
   cards: {
-    Emrakul: { id: 0, name: 'Emrakul, the Promised End', price: 22},
-    Kozilek: { id: 1, name: 'Kozilek, the Great Distortion', price: 4.49},
-    Ulamog: { id: 2, name: 'Ulamog, the Ceaseless Hunger', price: 12.50},
-    Bob: { id: 3, name: 'Dark Confidant', price: 41.29}
+    Emrakul: { id: 0, name: 'Emrakul, the Promised End', price: 22, image: '../client/images/emrakul.jpeg'},
+    Kozilek: { id: 1, name: 'Kozilek, the Great Distortion', price: 4.49, image: '../client/images/kozilek.jpeg'},
+    Ulamog: { id: 2, name: 'Ulamog, the Ceaseless Hunger', price: 12.50, image: '../client/images/ulamog.jpeg'},
+    Bob: { id: 3, name: 'Dark Confidant', price: 41.29, image: '../client/images/bob.jpeg'}
   },
   inventory: [],
   bank: 1000
